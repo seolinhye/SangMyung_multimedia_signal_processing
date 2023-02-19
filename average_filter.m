937b5dc5796625daebe325025faf2c35f634fdcc
설인혜 <tjfdlsgp123@naver.com>
2023-02-20 01:42:18 +0900

    convolution

---
+++ average_filter.m
@@ -0,0 +1,29 @@
+clear
+img = imread('lena2.bmp'); 
+[x,y] = size(img); 
+subplot(1,2,1); imshow(img);
+
+pad_img = zeros(x+2,y+2); 
+mask = ones(3)./9; 
+n = zeros(x,y); 
+
+%zero-padding
+for i = 2:x+1 
+  for j = 2:y+1
+    pad_img(i,j) = img(i-1,j-1); 
+ endfor
+endfor
+
+%convolution
+for a = 1:x
+  for b = 1:y
+    n(a,b) = sum(sum(mask.*(pad_img(a:a+2, b:b+2)))); 
+  endfor
+endfor
+
+result_img= uint8(n); 
+subplot(1,2,2); imshow(result_img);
+
+
+
+
